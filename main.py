5a2feebf74d3a886a63993e7325f338a3d188df2
HMB <johndoe@example.com>
2023-02-23 22:06:18 +0000

    sentiment and noun phrase analysis of random tweets

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import csv
 import random
-
+from textblob import TextBlob
 
 
 # Open the CSV file
@@ -11,12 +11,20 @@ with open('trumptweets.csv', encoding="utf8") as csvfile:
    # Loop through the rows in the CSV file
     row_count = 0
     for row in reader:
-        if random.random() < 0.001:
-            print(row[2])
+        if random.random() < 0.01:
+            blob = TextBlob(row[2])
+            sentiment_score = blob.sentiment.polarity
+            if sentiment_score > 0.5:      
+                print("Row number", row_count, "Sentiment score:", sentiment_score , "Noun_phrases", blob.noun_phrases, row[2])
         row_count += 1
     print(row_count)
-        
-        # Access the data in each row
-        # print(row)
+    
+
+# in order to run this programme you will need to install textblob in console
+# pip install textblob
+
+# to enable noun phrase and other analyses, we installed a further package in console
+# python -m textblob.download_corpora
+
 
 
